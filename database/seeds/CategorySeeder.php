8b4bda6406790863054bbf8234f43c75080f6bd8
Lauana Oliveira Hübler <lauanahubler@gmail.com>
2020-03-28 12:27:04 -0500

    Add code to categories table by new migration and in seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -12,18 +12,21 @@ class CategorySeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
+            'code' => 'GEN001',
             'name' => 'General',
             'description' => 'Productos Generales',
             'iva' => 0.19,
         ]);
 
         DB::table('categories')->insert([
+            'code' => 'EXE001',
             'name' => 'Exento',
             'description' => 'Productos exentos del impuesto',
             'iva' => 0,
         ]);
 
         DB::table('categories')->insert([
+            'code' => 'RED001',
             'name' => 'Reducido',
             'description' => 'Productos com impuesto reducido',
             'iva' => 0.05,
