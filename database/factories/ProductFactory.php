6b8d3fd1983c29e4120d8ac92abaf41ce0e3f315
Lauana Oliveira Hübler <lauanahubler@gmail.com>
2020-03-29 16:46:34 -0500

    Created migration, seeder and factory to Product model

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var Factory $factory */
+
+use App\Category;
+use App\Product;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'code' => strtoupper($faker->bothify('???###')),
+        'name' => $faker->sentence( 2, false),
+        'description' =>$faker->sentence( 6, true),
+        'price' => $faker->randomFloat(2, 0,50),
+        'category_id' => Category::all()->keyBy('id')->keys()->random(),
+    ];
+});
