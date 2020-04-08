a06a8e522c61fba0309d6c3eb95eefd0928b04b3
lau-hubler <lauanahubler@gmail.com>
2020-04-08 13:13:57 -0500

    add sidebar for nav

---
+++ resources/views/layouts/__sideBar.blade.php
@@ -0,0 +1,6 @@
+<b-list-group class="font-weight-bold">
+    <b-list-group-item href="{{route('invoices.index')}}">{{__('invoice.title')}}</b-list-group-item>
+    <b-list-group-item href="{{route('stakeholders.index')}}">{{__('stakeholder.title')}}</b-list-group-item>
+    <b-list-group-item href="{{route('products.index')}}">{{__('product.title')}}</b-list-group-item>
+    <b-list-group-item href="{{route('categories.index')}}">{{__('category.title')}}</b-list-group-item>
+</b-list-group>
